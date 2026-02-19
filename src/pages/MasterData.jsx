5264f30465acbbeb6d8a5b7c57b652e0aef97f7d
Akul Gupta <akul@toyokombucha.com>
2026-02-19 17:43:39 +0000

    File changes

--- src/pages/MasterData.jsx
+++ src/pages/MasterData.jsx
@@ -4,6 +4,7 @@ import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
 import BottleTypeManager from '@/components/master/BottleTypeManager';
 import LocationManager from '@/components/master/LocationManager';
 import MachineManager from '@/components/master/MachineManager';
+import AppSettingsManager from '@/components/master/AppSettingsManager';
 import { Loader2 } from 'lucide-react';
 
 export default function MasterData() {
