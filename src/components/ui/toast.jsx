f4f76807bc517adcfeae4cce6863419b47eb007c
Akul Gupta <akul@toyokombucha.com>
2026-02-26 17:58:15 +0000

    File changes

--- src/components/ui/toast.jsx
+++ src/components/ui/toast.jsx
@@ -15,7 +15,7 @@ ToastProvider.displayName = "ToastProvider";
 const ToastViewport = React.forwardRef(({ ...props }, ref) => (
   <div
     ref={ref}
-    className="fixed top-0 z-[100] flex max-h-screen w-full flex-col-reverse p-4 sm:bottom-0 sm:right-0 sm:top-auto sm:flex-col md:max-w-[420px]"
+    className="fixed bottom-0 right-0 z-[100] flex max-h-screen w-full flex-col p-4 pointer-events-none md:max-w-[420px]"
     {...props}
   />
 ));
@@ -101,4 +101,4 @@ export {
   ToastDescription,
   ToastClose,
   ToastAction,
-}; 
+};
