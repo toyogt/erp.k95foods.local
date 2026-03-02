4b2d8933d23b163e54e6c2ef79c6dc7fd49d79e8
Akul Gupta <akul@toyokombucha.com>
2026-03-02 13:04:54 +0000

    File changes

--- src/pages/LabellingLine.jsx
+++ src/pages/LabellingLine.jsx
@@ -12,6 +12,7 @@ import { logAudit } from '@/components/AuditLogger';
 import { raiseAlert } from '@/components/alerts/alertHelpers';
 import CrateTraceWindowPanel from '@/components/labelling/CrateTraceWindowPanel';
 import TraceSearchPanel from '@/components/labelling/TraceSearchPanel';
+import RollInstallPanel from '@/components/labelling/RollInstallPanel';
 
 const STEP = { MACHINE: 0, SELECT_WO: 1, SCAN_LABEL: 2, SCAN_CARTON: 3, CHECKLIST: 4, RUNNING: 5 };
 
