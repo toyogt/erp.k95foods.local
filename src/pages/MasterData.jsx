06094c28f0ef88bd8fb587e690f139fa3b85a456
Akul Gupta <akul@toyokombucha.com>
2026-03-03 14:39:27 +0000

    File changes

--- src/pages/MasterData.jsx
+++ src/pages/MasterData.jsx
@@ -56,7 +56,7 @@ export default function MasterData() {
         </div>
         <TabsContent value="products"   className="mt-4"><ProductMasterManager /></TabsContent>
         <TabsContent value="recipes"    className="mt-4"><RecipeMasterManager /></TabsContent>
-        <TabsContent value="ingredients" className="mt-4"><RecipeIngredientManager /></TabsContent>
+        <TabsContent value="ingredients" className="mt-4"><IngredientMasterManager /></TabsContent>
         <TabsContent value="bottles"    className="mt-4"><BottleTypeManager user={user} /></TabsContent>
         <TabsContent value="locations"  className="mt-4"><LocationManager user={user} /></TabsContent>
         <TabsContent value="machines"   className="mt-4"><MachineManager user={user} /></TabsContent>
