ded10f38f1c7c680ff4a322b9ddec76865d80793
Akul Gupta <akul@toyokombucha.com>
2026-04-04 06:36:36 +0000

    File changes

--- src/vite.config.js
+++ src/vite.config.js
@@ -8,10 +8,13 @@ export default defineConfig({
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
+      'react': path.resolve(__dirname, 'node_modules/react'),
+      'react-dom': path.resolve(__dirname, 'node_modules/react-dom'),
     },
     dedupe: ['react', 'react-dom', 'react/jsx-runtime'],
   },
   optimizeDeps: {
     include: ['react', 'react-dom', 'react/jsx-runtime'],
+    force: true,
   },
 });
