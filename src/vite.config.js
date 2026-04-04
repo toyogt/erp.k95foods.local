29aab4227400a61e6610e618ca0ca405193f9ad4
Akul Gupta <akul@toyokombucha.com>
2026-04-04 07:08:16 +0000

    File changes

--- src/vite.config.js
+++ src/vite.config.js
@@ -3,13 +3,5 @@ import react from '@vitejs/plugin-react';
 import base44Plugin from '@base44/vite-plugin';
 
 export default defineConfig({
-  plugins: [react(), base44Plugin()],
-  resolve: {
-    alias: {
-      '@': new URL('./src', import.meta.url).pathname,
-    },
-  },
-  optimizeDeps: {
-    disabled: true,
-  },
+  plugins: [base44Plugin(), react()],
 });
