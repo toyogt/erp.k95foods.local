212b4af832f52b90e2c50da3d20d98d6ac2e7fd8
Akul Gupta <akul@toyokombucha.com>
2026-04-04 06:20:44 +0000

    File changes

--- src/main.jsx
+++ src/main.jsx
@@ -3,14 +3,6 @@ import ReactDOM from 'react-dom/client'
 import App from '@/App.jsx'
 import '@/index.css'
 
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/sw.js')
-      .then(reg => console.log('SW Registered ✅', reg))
-      .catch(err => console.error('SW Registration Failed ❌', err));
-  });
-}
-
 ReactDOM.createRoot(document.getElementById('root')).render(
   <App />
 )
