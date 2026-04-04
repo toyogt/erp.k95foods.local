f0d25c2a5ac79ad1286b6c86f0ea8f344076367b
Akul Gupta <akul@toyokombucha.com>
2026-04-04 10:14:58 +0000

    File changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import { Toaster } from "@/components/ui/toaster"
 import { ToastContextProvider } from "@/components/ui/toast-provider"
 import { QueryClientProvider } from '@tanstack/react-query'
@@ -254,9 +255,9 @@ function App() {
           <Router>
             <NavigationTracker />
             <AuthenticatedApp />
-            <Toaster />
           </Router>
         </ToastContextProvider>
+        <Toaster />
       </QueryClientProvider>
     </AuthProvider>
   )
