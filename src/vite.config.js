7447cc85c8064336637bc1e21a5add9ecb6afe5c
Akul Gupta <akul@toyokombucha.com>
2026-04-04 08:15:44 +0000

    File changes

--- src/vite.config.js
+++ src/vite.config.js
@@ -1,1 +1,26 @@
-//We need some corrections to this file
+import base44 from "@base44/vite-plugin"
+import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
+import path from 'path'
+
+export default defineConfig({
+  logLevel: 'error',
+  plugins: [
+    base44({
+      legacySDKImports: process.env.BASE44_LEGACY_SDK_IMPORTS === 'true',
+      hmrNotifier: true,
+      navigationNotifier: true,
+      visualEditAgent: true
+    }),
+    react(),
+  ],
+  server: {
+    allowedHosts: ['**'],
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    },
+    dedupe: ['react', 'react-dom'],
+  },
+});
