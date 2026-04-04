51422c4d4bf7be9aeb73db72ceee429142e6f40f
Akul Gupta <akul@toyokombucha.com>
2026-04-04 10:09:09 +0000

    File changes

--- src/App.jsx
+++ src/App.jsx
@@ -338,8 +338,8 @@ function App() {
         <Router>
           <NavigationTracker />
           <AuthenticatedApp />
+          <Toaster />
         </Router>
-        <Toaster />
       </QueryClientProvider>
     </AuthProvider>
   )
