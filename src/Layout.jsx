af606fcf4add1f60354b43d15c7acae783262497
Akul Gupta <akul@toyokombucha.com>
2026-04-07 06:39:01 +0000

    File changes

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -259,7 +259,7 @@ export default function Layout({ children, currentPageName }) {
             )}
           </header>
 
-          <main className="flex-1 px-2 md:px-4 py-4 pb-24 w-full">
+          <main className="flex-1 px-1 md:px-2 py-3 pb-24 w-full">
             {children}
           </main>
         </div>
